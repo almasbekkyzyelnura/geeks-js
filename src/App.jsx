9b16a82b947a55d5c43cabe52deab8d00a2fdc75
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-07-27 18:03:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import  { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+        const data = await response.json();
+        const detailedPokemonPromises = data.results.map(pokemon =>
+          fetch(pokemon.url).then(res => res.json())
+        );
+        const detailedPokemons = await Promise.all(detailedPokemonPromises);
+        setPokemonData(detailedPokemons);
+      } catch (error) {
+        console.error('Ошибка при загрузке данных:', error);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div className='div'>
+      <div className='container'>
+        <h2>GEEKS</h2>
+        <div className='card'>
+          {pokemonData.map((pokemon) => (
+            <div className='bloc' key={pokemon.id}>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pokemon-image' />
+              <h3>{pokemon.name}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
